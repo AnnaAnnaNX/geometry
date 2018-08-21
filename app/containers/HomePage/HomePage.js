5531d253c6e410082dc76d4ce023b05b846c6d44
anna <anna.annanx@rambler.ru>
2018-08-21 19:05:14 +0300

    add cube with defined user paramaters

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -9,17 +9,56 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import ReposList from 'components/ReposList';
 import ThreeScene from './ThreeScene';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import './style.scss';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
    * when initial state username is not null, submit the form to load repos
    */
+   constructor(props) {
+    super(props);
+    this.state = {
+      x: null,
+      y: null,
+      z: null,
+      w: null,
+      h: null,
+      d: null,
+      points: undefined 
+      // [
+      //   [0, 0, 0],
+      //   [1, 0, 0],
+      //   [1, 1, 0],
+      //   [0, 1, 0],
+      //   [0, 0, 1],
+      //   [1, 0, 1],
+      //   [1, 1, 1],
+      //   [0, 1, 1],
+      // ]
+      ,
+
+
+
+
+      width: 1,
+      height: 2,
+      long: 3
+    };
+  } 
+
   componentDidMount() {
     if (this.props.username && this.props.username.trim().length > 0) {
       this.props.onSubmitForm();
     }
   }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
   render() {
     const { loading, error, repos } = this.props;
@@ -43,14 +82,87 @@ function draw() {
   pop();
 }
 
-
     return (
       <article>
         <Helmet>
           <title>Home Page</title>
           <meta name="description" content="A React.js Boilerplate application homepage" />
         </Helmet>
-        <ThreeScene></ThreeScene>
+
+        <div>
+            <h3>Cube properties:</h3>
+            <p>
+            Position{' '}
+            <TextField style={{width: '20px'}}
+                id="x"
+                label="x"
+                value={this.state.x}
+                onChange={this.handleChange('x')}
+            />
+            <TextField style={{width: '20px'}}
+                id="y"
+                label="y"
+                value={this.state.y}
+                onChange={this.handleChange('y')}
+            />
+            <TextField style={{width: '20px'}}
+                id="z"
+                label="z"
+                value={this.state.z}
+                onChange={this.handleChange('z')}
+            />
+            </p>
+
+            <p>
+            Cube's parameters{' '}
+            <TextField style={{width: '20px'}}
+                id="w"
+                label="w"
+                value={this.state.w}
+                onChange={this.handleChange('w')}
+            />
+            <TextField style={{width: '20px'}}
+                id="h"
+                label="h"
+                value={this.state.h}
+                onChange={this.handleChange('h')}
+            />
+            <TextField style={{width: '20px'}}
+                id="d"
+                label="d"
+                value={this.state.d}
+                onChange={this.handleChange('d')}
+            />
+            </p>
+            <Button variant="contained" color="primary"
+              onClick={()=>{
+                console.log('x', this.state.x);
+
+                const x = this.state.x,
+                 y = this.state.y,
+                 z = this.state.z,
+                 w = this.state.w,
+                 h = this.state.h,
+                 d = this.state.d;
+
+                const points = [
+                  [x, y, z],
+                  [x+w, y, z],
+                  [x+w, y+h, z],
+                  [x, y+h, z],
+                  [x, y, z+d],
+                  [x+w, y, z+d],
+                  [x+w, y+h, z+d],
+                  [x, y+h, z+d],
+                ];
+
+                this.setState({points: points});
+
+              }}
+            >Add cube</Button>
+        </div>
+
+        <ThreeScene points = {this.state.points}></ThreeScene>
         <div className="home-page">
 
         </div>
--- app/containers/HomePage/ThreeScene.js
+++ app/containers/HomePage/ThreeScene.js
@@ -4,66 +4,31 @@ class ThreeScene extends Component{
   constructor(props) {
     super(props);
     this.state = {
+
       width: 1,
       height: 2,
-      long: 3
+      long: 3,
+      points: this.props.points
     };
   }
 
-  componentDidMount(){
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
-    //ADD SCENE
-    this.scene = new THREE.Scene()
-    //ADD CAMERA
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    )
-    this.camera.position.z = 4
-    //ADD RENDERER
-    this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setClearColor('#000000')
-    this.renderer.setSize(width, height)
-    this.mount.appendChild(this.renderer.domElement)
-    // //ADD CUBE
-    // const geometry = new THREE.BoxGeometry(this.state.width, this.state.height, this.state.long)
-    // const material = new THREE.MeshBasicMaterial({ color: '#433F81'     })
-    // this.cube = new THREE.Mesh(geometry, material)
-    
+  componentWillReceiveProps({points}) {console.log('new props');
+    this.setState({...this.state,points})
+  }
 
-    // console.log('this.cube.position',this.cube.position)//!position
-    // console.log('this.cube',this.cube)//!matrix
-    // console.log('this.cube.toJSON()',this.cube.toJSON())//!matrix
-
-    // this.scene.add(this.cube)
-
-
-
-    //ADD SHAPE FROM POINTS
-    let vertices = [
-      new THREE.Vector3(0, 0, 0),
-      new THREE.Vector3(1, 0, 0),
-      new THREE.Vector3(1, 1, 0),
-      new THREE.Vector3(0, 1, 0),
-      new THREE.Vector3(0, 0, 1),
-      new THREE.Vector3(1, 0, 1),
-      new THREE.Vector3(1, 1, 1),
-      new THREE.Vector3(0, 1, 1),
-    ]
-    let holes = []
-    let triangles, mesh;
+  draw = () => {
     let geometry = new THREE.Geometry();
     // let material = new THREE.MeshNormalMaterial()
     let material = new THREE.MeshBasicMaterial({
         color: 0xff0000,
         wireframe: true
     });
+    
+    let vertices = [0,1,2,3,4,5,6,7].map(i=>new THREE.Vector3(...this.props.points[i]))
+    let holes = []
+    let triangles, mesh;
 
-
-    geometry.vertices = vertices;
+        geometry.vertices = vertices;
 
     triangles = THREE.ShapeUtils.triangulateShape ( vertices, holes );
     console.log('triangles', triangles);
@@ -96,6 +61,84 @@ class ThreeScene extends Component{
 // this.start()
 this.renderScene()
   }
+
+  componentDidMount(){
+    const width = this.mount.clientWidth
+    const height = this.mount.clientHeight
+    //ADD SCENE
+    this.scene = new THREE.Scene()
+    //ADD CAMERA
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      1000
+    )
+    this.camera.position.z = 4
+    //ADD RENDERER
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setClearColor('#000000')
+    this.renderer.setSize(width, height)
+    this.mount.appendChild(this.renderer.domElement)
+    // //ADD CUBE
+    // const geometry = new THREE.BoxGeometry(this.state.width, this.state.height, this.state.long)
+    // const material = new THREE.MeshBasicMaterial({ color: '#433F81'     })
+    // this.cube = new THREE.Mesh(geometry, material)
+    
+
+    // console.log('this.cube.position',this.cube.position)//!position
+    // console.log('this.cube',this.cube)//!matrix
+    // console.log('this.cube.toJSON()',this.cube.toJSON())//!matrix
+
+    // this.scene.add(this.cube)
+
+console.log('this.props.points', this.props.points);
+
+    //ADD SHAPE FROM POINTS
+    // let vertices = [0,1,2,3,4,5,6,7].map(i=>new THREE.Vector3(...this.props.points[i]))
+    // let holes = []
+    // let triangles, mesh;
+    // let geometry = new THREE.Geometry();
+    // // let material = new THREE.MeshNormalMaterial()
+    // let material = new THREE.MeshBasicMaterial({
+    //     color: 0xff0000,
+    //     wireframe: true
+    // });
+
+
+    // geometry.vertices = vertices;
+
+    // triangles = THREE.ShapeUtils.triangulateShape ( vertices, holes );
+    // console.log('triangles', triangles);
+
+    // // for( var i = 0; i < triangles.length; i++ ){
+    // //     geometry.faces.push( new THREE.Face3( triangles[i][0], triangles[i][1], triangles[i][2] ));
+    // // }
+
+    // geometry.faces.push(new THREE.Face3(0,1,2));//[0] - сторона 0 (front)
+    // geometry.faces.push(new THREE.Face3(0,2,3));//[1] - сторона 0 (front)
+    // geometry.faces.push(new THREE.Face3(1,5,6));//[2] - сторона 1 (right) 
+    // geometry.faces.push(new THREE.Face3(1,6,2));//[3] - сторона 1 (right)
+    // geometry.faces.push(new THREE.Face3(3,2,6));//[2] - сторона 2 (top) 
+    // geometry.faces.push(new THREE.Face3(3,6,7));//[3] - сторона 2 (top)
+    // geometry.faces.push(new THREE.Face3(0,4,7));//[2] - сторона 3 (left) 
+    // geometry.faces.push(new THREE.Face3(0,7,3));//[3] - сторона 3 (left)
+    // geometry.faces.push(new THREE.Face3(0,1,5));//[2] - сторона 4 (bottom) 
+    // geometry.faces.push(new THREE.Face3(0,5,4));//[3] - сторона 4 (bottom)
+    // geometry.faces.push(new THREE.Face3(4,5,6));//[2] - сторона 5 (back) 
+    // geometry.faces.push(new THREE.Face3(4,6,7));//[3] - сторона 5 (back)
+
+    // console.log('geometry.faces', geometry.faces)
+
+    // this.mesh = new THREE.Mesh( geometry, material );
+    
+    // console.log('this.mesh', this.mesh)
+
+    // this.scene.add(this.mesh)
+
+// this.start()
+this.renderScene()
+  }
 componentWillUnmount(){
     this.stop()
     this.mount.removeChild(this.renderer.domElement)
@@ -117,7 +160,8 @@ animate = () => {
 renderScene = () => {
   this.renderer.render(this.scene, this.camera)
 }
-render(){
+render(){console.log('new render ThreeScene');
+  if (this.props.points) {console.log('вызов draw');this.draw();}
     return(
       <div>
         <div
