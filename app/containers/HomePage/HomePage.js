6359b496922a67626a4a5205cd178063707337f5
anna <anna.annanx@rambler.ru>
2018-08-21 19:42:12 +0300

    add controls for select faces

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -11,6 +11,12 @@ import ReposList from 'components/ReposList';
 import ThreeScene from './ThreeScene';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
 import './style.scss';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -44,6 +50,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
       //   [0, 1, 1],
       // ]
       ,
+      selectedFaces: [],
 
 
 
@@ -64,6 +71,15 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     this.setState({
       [name]: event.target.value,
     });
+  };
+
+ handleChange = name => event => {
+    if (!this.state.selectedFaces.includes(name))
+      this.setState({ selectedFaces: [...this.state.selectedFaces, name] })
+    else{
+        this.setState({ selectedFaces: this.state.selectedFaces.filter( i => i != name ) });
+    }
+
   };
 
   render() {
@@ -96,49 +112,96 @@ function draw() {
         </Helmet>
 
         <div>
-            <h3>Cube properties:</h3>
-            <p>
-            Position{' '}
-            <TextField style={{width: '20px'}}
-                id="x"
-                label="x"
-                value={this.state.x}
-                onChange={this.handleChange('x')}
-            />
-            <TextField style={{width: '20px'}}
-                id="y"
-                label="y"
-                value={this.state.y}
-                onChange={this.handleChange('y')}
-            />
-            <TextField style={{width: '20px'}}
-                id="z"
-                label="z"
-                value={this.state.z}
-                onChange={this.handleChange('z')}
-            />
-            </p>
+            <h3>Cube properties</h3>
+            <span>
+              Position:{' '}
+              <TextField style={{width: '20px'}}
+                  id="x"
+                  label="x"
+                  value={this.state.x}
+                  onChange={this.handleChange('x')}
+              />
+              <TextField style={{width: '20px'}}
+                  id="y"
+                  label="y"
+                  value={this.state.y}
+                  onChange={this.handleChange('y')}
+              />
+              <TextField style={{width: '20px'}}
+                  id="z"
+                  label="z"
+                  value={this.state.z}
+                  onChange={this.handleChange('z')}
+              />
+            </span>
+
+            <span>
+              Cube's parameters:{' '}
+              <TextField style={{width: '20px'}}
+                  id="w"
+                  label="w"
+                  value={this.state.w}
+                  onChange={this.handleChange('w')}
+              />
+              <TextField style={{width: '20px'}}
+                  id="h"
+                  label="h"
+                  value={this.state.h}
+                  onChange={this.handleChange('h')}
+              />
+              <TextField style={{width: '20px'}}
+                  id="d"
+                  label="d"
+                  value={this.state.d}
+                  onChange={this.handleChange('d')}
+              />
+            </span>
 
             <p>
-            Cube's parameters{' '}
-            <TextField style={{width: '20px'}}
-                id="w"
-                label="w"
-                value={this.state.w}
-                onChange={this.handleChange('w')}
-            />
-            <TextField style={{width: '20px'}}
-                id="h"
-                label="h"
-                value={this.state.h}
-                onChange={this.handleChange('h')}
-            />
-            <TextField style={{width: '20px'}}
-                id="d"
-                label="d"
-                value={this.state.d}
-                onChange={this.handleChange('d')}
-            />
+              Select faces:
+                <div style={{display: 'flex'}}>
+                 <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Checkbox checked={this.state.selectedFaces.includes(0)} onChange={this.handleChange(0)} />
+                    }
+                    label="0 - front"//front
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox checked={this.state.selectedFaces.includes(1)} onChange={this.handleChange(1)} />
+                    }
+                    label="1 -top"//top
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox checked={this.state.selectedFaces.includes(2)} onChange={this.handleChange(2)} />
+                    }
+                    label="2 -right"//top
+                  />
+                </FormGroup>
+
+                 <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Checkbox checked={this.state.selectedFaces.includes(3)} onChange={this.handleChange(3)} />
+                    }
+                    label="3 - left"//front
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox checked={this.state.selectedFaces.includes(4)} onChange={this.handleChange(4)} />
+                    }
+                    label="4 - bottom"//top
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox checked={this.state.selectedFaces.includes(5)} onChange={this.handleChange(5)} />
+                    }
+                    label="5 - back"//top
+                  />
+                </FormGroup>
+              </div>
             </p>
             <Button variant="contained" color="primary"
               onClick={()=>{
